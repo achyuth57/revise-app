d9ac62276553f210bbae6bba689d73a14d0d53d5
Vaddi Achyuth Kumar <va20079148@D-C02Q704KF8J2.local>
2019-06-18 11:08:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/form'
 
 function App() {
   return (
     <div className="App">
        <h1>Hello React</h1>
+       <Form />
     </div>
   );
 }
